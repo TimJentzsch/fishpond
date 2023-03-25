bc02b6b114cccda6f18557b5a5b971817bb3005a
Tim Jentzsch <tim-jentzsch@gmx.de>
2023-03-25 22:42:20 +0100

    Add checkbox to flip board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script>
 	import BoardView from '../lib/components/BoardView.svelte';
+
+	let flipped = true;
 </script>
 
 <h1>Fishpond</h1>
 
 <div class="board-container">
-	<BoardView />
+	<BoardView {flipped} />
+	<label>
+		<input type="checkbox" bind:checked={flipped} />
+		Flip Board
+	</label>
 </div>
 
 <style>
