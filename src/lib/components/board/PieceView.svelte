800180c63def223e08168e3ca58585730a45b30c
Tim Jentzsch <commits@timjen.net>
2023-04-04 20:54:03 +0200

    Disable image dragging when piece cannot be dragged

--- src/lib/components/board/PieceView.svelte
+++ src/lib/components/board/PieceView.svelte
@@ -10,12 +10,17 @@
 	$: src = `/piece/${pieceSet}/${piece.color}${piece.type.toUpperCase()}.svg`;
 </script>
 
-<img class="piece" {src} alt={symbol} hidden={!isVisible} />
+<img class="piece" {src} alt={symbol} hidden={!isVisible} draggable="false" />
 
 <style>
 	.piece {
 		width: 100%;
 		height: 100%;
+
+		-webkit-user-drag: none;
 		user-select: none;
+		-moz-user-select: none;
+		-webkit-user-select: none;
+		-ms-user-select: none;
 	}
 </style>
