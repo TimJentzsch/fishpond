2d643079b1c03379361eaedc5bbc148ac800cdeb
Tim Jentzsch <commits@timjen.net>
2023-12-23 00:00:34 +0100

    Add option for multiple processes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,19 +4,30 @@
 )]
 
 use std::{
+    collections::HashMap,
     io::{BufRead, BufReader, BufWriter, Write},
-    process::{Command, Stdio},
+    process::{Child, Command, Stdio},
+    sync::Mutex,
 };
+
+use tauri::State;
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![run_engine])
+        .manage(EngineProcesses {
+            processes: Default::default(),
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
+struct EngineProcesses {
+    processes: Mutex<HashMap<String, Child>>,
+}
+
 #[tauri::command]
-fn run_engine() {
+fn run_engine(state: State<EngineProcesses>) {
     println!("Running engine!");
 
     let cmd = Command::new("stockfish")
@@ -24,6 +35,12 @@ fn run_engine() {
         .stdout(Stdio::piped())
         .spawn()
         .unwrap();
+
+    state
+        .processes
+        .lock()
+        .unwrap()
+        .insert("stockfish".to_string(), cmd);
 
     if let Some(std_in) = cmd.stdin {
         let mut writer = BufWriter::new(std_in);
