769744c0a5f5684ae98ce40c43ae3c60d3c3ac8b
Tim Jentzsch <commits@timjen.net>
2024-01-09 23:26:37 +0100

    Send uci input to process

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::process::Command;
 
 use bevy::prelude::*;
-use bevy_local_commands::{BevyLocalCommandsPlugin, LocalCommand, ProcessOutput};
+use bevy_local_commands::{BevyLocalCommandsPlugin, LocalCommand, Process, ProcessOutput};
 
 fn main() {
     App::new()
         .add_plugins((MinimalPlugins, BevyLocalCommandsPlugin))
         .add_systems(Startup, start_stockfish)
-        .add_systems(Update, log_output)
+        .add_systems(Update, (log_output, uci).chain())
         .run();
 }
 
@@ -18,5 +18,11 @@ fn start_stockfish(mut commands: Commands) {
 fn log_output(mut output_event: EventReader<ProcessOutput>) {
     for output in output_event.read() {
         println!("{}", output.output.join("\n"));
+    }
+}
+
+fn uci(mut query: Query<&mut Process, Added<Process>>) {
+    for mut process in query.iter_mut() {
+        process.println("uci").unwrap();
     }
 }
