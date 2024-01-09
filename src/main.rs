cd98db9eea7891a1426ad2c75065d646eb91ad50
Tim Jentzsch <commits@timjen.net>
2024-01-09 19:39:57 +0100

    Lunch Stockfish and log its output

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use std::process::Command;
+
 use bevy::prelude::*;
-use bevy_local_commands::BevyLocalCommandsPlugin;
+use bevy_local_commands::{BevyLocalCommandsPlugin, LocalCommand, ProcessOutput};
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, BevyLocalCommandsPlugin))
+        .add_plugins((MinimalPlugins, BevyLocalCommandsPlugin))
+        .add_systems(Startup, start_stockfish)
+        .add_systems(Update, log_output)
         .run();
+}
+
+fn start_stockfish(mut commands: Commands) {
+    commands.spawn(LocalCommand::new(Command::new("stockfish")));
+}
+
+fn log_output(mut output_event: EventReader<ProcessOutput>) {
+    for output in output_event.read() {
+        println!("{}", output.output.join("\n"));
+    }
 }
