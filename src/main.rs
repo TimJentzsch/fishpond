04ec26153f796780a02fa8e5f0cd915fd6e58007
Tim Jentzsch <commits@timjen.net>
2024-01-12 19:21:49 +0100

    Basic UCI initialization

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,20 @@ use bevy_local_commands::{
     BevyLocalCommandsPlugin, LocalCommand, Process, ProcessError, ProcessOutput,
 };
 
+#[derive(Debug, Default, Component)]
+struct Game;
+
+#[derive(Debug, Component)]
+struct GameRef(Entity);
+
+#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Component)]
+enum EngineState {
+    #[default]
+    Startup,
+    UciInit,
+    Ready,
+}
+
 fn main() {
     App::new()
         .add_plugins((
@@ -15,12 +29,49 @@ fn main() {
             BevyLocalCommandsPlugin,
         ))
         .add_systems(Startup, start_stockfish)
-        .add_systems(Update, ((log_output, log_errors), uci).chain())
+        .add_systems(
+            Update,
+            (
+                log_output,
+                log_errors,
+                handle_engine_startup,
+                handle_engine_uci_init,
+            ),
+        )
         .run();
 }
 
 fn start_stockfish(mut commands: Commands) {
-    commands.spawn(LocalCommand::new("stockfish"));
+    let game_id = commands.spawn(Game).id();
+    commands.spawn((
+        EngineState::default(),
+        GameRef(game_id),
+        LocalCommand::new("stockfish"),
+    ));
+}
+
+fn handle_engine_startup(mut state_query: Query<(&mut EngineState, &mut Process), Added<Process>>) {
+    for (mut state, mut process) in state_query.iter_mut() {
+        println!("Initializing UCI...");
+        process.println("uci").expect("Failed to send uci command");
+        *state = EngineState::UciInit;
+    }
+}
+
+fn handle_engine_uci_init(
+    mut output_event: EventReader<ProcessOutput>,
+    mut state_query: Query<&mut EngineState>,
+) {
+    for output in output_event.read() {
+        for line in &output.output {
+            if line.trim().starts_with("uciok") {
+                if let Ok(mut state) = state_query.get_mut(output.entity) {
+                    println!("Engine ready!");
+                    *state = EngineState::Ready;
+                }
+            }
+        }
+    }
 }
 
 fn log_output(mut output_event: EventReader<ProcessOutput>) {
@@ -32,11 +83,5 @@ fn log_output(mut output_event: EventReader<ProcessOutput>) {
 fn log_errors(mut error_event: EventReader<ProcessError>) {
     for error in error_event.read() {
         eprintln!("{error:?}");
-    }
-}
-
-fn uci(mut query: Query<&mut Process, Added<Process>>) {
-    for mut process in query.iter_mut() {
-        process.println("uci").unwrap();
     }
 }
