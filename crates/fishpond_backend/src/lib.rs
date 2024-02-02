57092719fe93abddfbffaa4b8a0e3b5c2fb476c0
TimJentzsch <commits@timjen.net>
2024-02-02 14:09:06 +0100

    Move backend to separate crate (#41)
    * Create crate for backend library
    
    * Move backend to fishpond_backend crate

---
+++ crates/fishpond_backend/src/lib.rs
@@ -0,0 +1,27 @@
+use bevy::prelude::*;
+use bevy_local_commands::BevyLocalCommandsPlugin;
+use chess::{CreateGame, GamePlugin};
+use engine::EnginePlugin;
+use process_log::ProcessLogPlugin;
+
+mod chess;
+mod engine;
+mod process_log;
+
+pub struct FishpondBackendPlugin;
+
+impl Plugin for FishpondBackendPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins((
+            BevyLocalCommandsPlugin,
+            ProcessLogPlugin,
+            EnginePlugin,
+            GamePlugin,
+        ))
+        .add_systems(Startup, create_game);
+    }
+}
+
+fn create_game(mut create_game_event: EventWriter<CreateGame>) {
+    create_game_event.send(CreateGame);
+}
---
+++ crates/fishpond_backend/src/main.rs
@@ -0,0 +1,31 @@
+use std::time::Duration;
+
+use bevy::{app::ScheduleRunnerPlugin, prelude::*};
+use bevy_local_commands::BevyLocalCommandsPlugin;
+use chess::{CreateGame, GamePlugin};
+use engine::EnginePlugin;
+use process_log::ProcessLogPlugin;
+
+mod chess;
+mod engine;
+mod process_log;
+
+fn main() {
+    App::new()
+        .add_plugins((
+            MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(
+                // Limit to 30 FPS
+                1.0 / 30.0,
+            ))),
+            BevyLocalCommandsPlugin,
+            ProcessLogPlugin,
+            EnginePlugin,
+            GamePlugin,
+        ))
+        .add_systems(Startup, create_game)
+        .run();
+}
+
+fn create_game(mut create_game_event: EventWriter<CreateGame>) {
+    create_game_event.send(CreateGame);
+}
