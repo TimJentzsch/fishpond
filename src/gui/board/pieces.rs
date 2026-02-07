a876d7d6d828f42ad4856ab27e37a0ed0d2fea07
Tim Jentzsch <commits@timjen.net>
2026-02-07 15:28:16 +0100

    Also efficiently update captures and promotions

--- src/gui/board/pieces.rs
+++ src/gui/board/pieces.rs
@@ -32,7 +32,7 @@ pub fn update_pieces(
     mut commands: Commands,
     game_query: Query<&Game<Chess>>,
     mut piece_container_query: Query<(Entity, Option<&mut RenderedPosition>), With<PieceContainer>>,
-    mut piece_query: Query<(&mut Node, &mut RenderedPiece)>,
+    mut piece_query: Query<(Entity, &mut Node, &mut ImageNode, &mut RenderedPiece)>,
     asset_server: Res<AssetServer>,
 ) {
     let Ok(game) = game_query.single() else {
@@ -62,19 +62,52 @@ pub fn update_pieces(
                     to,
                     promotion,
                 } = *last_move
-                    && capture.is_none()
                     && promotion.is_none()
                 {
                     // Try to find the matching piece
-                    for (mut node, mut rendered_piece) in piece_query.iter_mut() {
+                    for (_, mut node, mut image_node, mut rendered_piece) in piece_query.iter_mut()
+                    {
                         if rendered_piece.square == from && rendered_piece.piece.role == role {
                             // Move the piece to the new square
                             rendered_piece.square = to;
                             set_square_position(&mut node, to);
-                            println!("---- EFFICIENT UPDATE ----");
-                            return;
+
+                            if let Some(promotion) = promotion {
+                                rendered_piece.piece.role = promotion;
+
+                                // Update the piece image
+                                let piece_color = match rendered_piece.piece.color {
+                                    shakmaty::Color::White => "w",
+                                    shakmaty::Color::Black => "b",
+                                };
+                                let piece_type = match promotion {
+                                    shakmaty::Role::Pawn => "P",
+                                    shakmaty::Role::Knight => "N",
+                                    shakmaty::Role::Bishop => "B",
+                                    shakmaty::Role::Rook => "R",
+                                    shakmaty::Role::Queen => "Q",
+                                    shakmaty::Role::King => "K",
+                                };
+                                let piece_image_path =
+                                    format!("pieces/cburnett/{piece_color}{piece_type}.png");
+                                image_node.image = asset_server.load(piece_image_path);
+                            }
+                            break;
                         }
                     }
+
+                    if let Some(capture) = capture {
+                        // Remove the captured piece
+                        for (entity, _, _, rendered_piece) in piece_query.iter_mut() {
+                            if rendered_piece.square == to && rendered_piece.piece.role == capture {
+                                commands.entity(entity).despawn();
+                                break;
+                            }
+                        }
+                    }
+
+                    println!("---- EFFICIENT UPDATE ----");
+                    return;
                 }
             }
             visualized_position.0 = game.current_position().clone();
